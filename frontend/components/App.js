5a916a444cc7e2102c73bb16d71f53092951839f
PalmTreeBreeZee <azeembrown717@gmail.com>
2024-05-06 10:48:30 -0400

    nav

---
+++ frontend/components/App.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Home from './Home'
+import Form from './Form'
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
+
+function App() {
+  return (
+    <div id="app">
+      <BrowserRouter>
+      <nav>
+        {/* NavLinks here */}
+        <Link to="/" class='active'>Home</Link>
+        <Link to="order" class=''>Order</Link>
+      </nav>
+      {/* Route and Routes here */}
+      <Routes>
+      <Route path='/' element={<Home />}/>
+      <Route path='order' element={<Form />}/>
+      </Routes>
+    </BrowserRouter>
+    </div>
+    
+  )
+}
+
+export default App
